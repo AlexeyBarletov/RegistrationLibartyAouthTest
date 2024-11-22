55a60fbe8adfe164d96824e746ccef6239368d1a
Alexey Barletov <barletov_92@inbox.ru>
2024-11-22 13:40:11 +0500

    init

---
+++ RegistrationTest/RegistrationTest/RegistrationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationTestApp.swift
+//  RegistrationTest
+//
+//  Created by Алексей Барлетов on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
